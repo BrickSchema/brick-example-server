ce87742e6a096ac2336c1301d5099cfc5dea441c
Jason Koh <bk7749@gmail.com>
2019-01-15 15:57:03 -0800

    make the components configurable

--- brick_server/__init__.py
+++ brick_server/__init__.py
@@ -1,6 +1,5 @@
 import pdb
-from .services.data.resources import TimeseriesById
-from .services.data.resources import Timeseries
+import json
 
 from flask import Flask
 from flask_injector import FlaskInjector
@@ -11,6 +10,7 @@ from apispec_webframeworks.flask import FlaskPlugin
 from .apis import blueprint
 
 def configure_binding(binder):
+    configs = json.load(open('configs/configs.json'))
     dbname = 'brick'
     user = 'bricker'
     pw = 'brick-demo'
@@ -19,10 +19,25 @@ def configure_binding(binder):
     from brick_data.timeseries import BrickTimeseries
     from brick_data.sparql import BrickSparql
     from brick_server.extensions.lockmanager import LockManager
-    lock_manager = LockManager(host, port, dbname, user, pw)
-    brick_ts = BrickTimeseries(dbname, user, pw, host, port)
-    brick_sparql = BrickSparql('http://localhost:8890/sparql', '1.0.3',
-                               base_ns='http://jason.com/')
+    brick_ts_configs = configs['timeseries']
+    brick_ts = BrickTimeseries(brick_ts_configs['dbname'],
+                               brick_ts_configs['user'],
+                               brick_ts_configs['password'],
+                               brick_ts_configs['host'],
+                               brick_ts_configs['port'],
+                               )
+    lockmanager_configs = configs['lockmanager']
+    lock_manager = LockManager(lockmanager_configs['host'],
+                               lockmanager_configs['port'],
+                               lockmanager_configs['dbname'],
+                               lockmanager_configs['user'],
+                               lockmanager_configs['password'],
+                               )
+    brick_configs = configs['brick']
+    brick_sparql = BrickSparql(brick_configs['host'],
+                               brick_configs['brick_version'],
+                               base_ns=brick_configs['base_ns'],
+                               )
     binder.bind(BrickTimeseries, to=brick_ts)
     binder.bind(BrickSparql, to=brick_sparql)
     binder.bind(LockManager, to=lock_manager)
@@ -31,16 +46,4 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(blueprint, url_prefix='/api/v1')
     FlaskInjector(app=app, modules=[configure_binding])
-
-    spec = APISpec(
-        title='Brick Server',
-        version='1.0.0',
-        openapi_version='3.0',
-        plugins=[
-            FlaskPlugin(),
-            MarshmallowPlugin(),
-        ],
-    )
-
-
     return app
