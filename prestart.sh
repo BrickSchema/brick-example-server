f02e231c52120aace998ab5a0ea2bfaad6259a4e
Jason Koh <bk7749@gmail.com>
2020-02-28 03:28:28 +0000

    update

--- prestart.sh
+++ prestart.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-ssh-keygen -t rsa -N '' -b 4096 -m PEM -f configs/jwtRS256.key
-openssl rsa -in configs/jwtRS256.key -pubout -outform PEM -out configs/jwtRS256.key.pub
+mkdir /app/configs
+
+ssh-keygen -t rsa -N '' -b 4096 -m PEM -f /app/configs/jwtRS256.key
+openssl rsa -in /app/configs/jwtRS256.key -pubout -outform PEM -out /app/configs/jwtRS256.key.pub
