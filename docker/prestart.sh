844c5ccf9fec726f016e8c6ba5f83927fb31c79d
Jason Koh <bk7749@gmail.com>
2020-02-29 19:51:08 +0000

    update

--- docker/prestart.sh
+++ docker/prestart.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /app/configs
 
-if [ -f /app/configs/jwtRS256.key]; then
-  if [ -f /app/configs/jwtRS256.key.pub]; then
+if [ -f /app/configs/jwtRS256.key ]; then
+  if [ -f /app/configs/jwtRS256.key.pub ]; then
     echo "Reusing existing JWT key"
   else
     echo "JWT private key is given but not its public key"
@@ -12,5 +12,5 @@ if [ -f /app/configs/jwtRS256.key]; then
 else
   ssh-keygen -t rsa -N '' -b 4096 -m PEM -f /app/configs/jwtRS256.key
   openssl rsa -in /app/configs/jwtRS256.key -pubout -outform PEM -out /app/configs/jwtRS256.key.pub
-  "JWT private and public keys are created."
+  echo "JWT private and public keys are created."
 fi
