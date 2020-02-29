4c27bd910519ca9cf237479c1b4e9e31fc444054
Jason Koh <bk7749@gmail.com>
2020-02-29 03:22:11 +0000

    add docker scripts

---
+++ docker/prestart.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+mkdir -p /app/configs
+
+ssh-keygen -t rsa -N '' -b 4096 -m PEM -f /app/configs/jwtRS256.key
+openssl rsa -in /app/configs/jwtRS256.key -pubout -outform PEM -out /app/configs/jwtRS256.key.pub
