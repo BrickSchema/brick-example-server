d987c4112a0d114ee22f29f9946f7e627cbe7428
Jason Koh <bk7749@gmail.com>
2020-02-29 19:43:53 +0000

    fix a bug

--- docker/prestart.sh
+++ docker/prestart.sh
@@ -8,7 +8,9 @@ if [ -f /app/configs/jwtRS256.key]; then
   else
     echo "JWT private key is given but not its public key"
     exit 1
+  fi
 else
   ssh-keygen -t rsa -N '' -b 4096 -m PEM -f /app/configs/jwtRS256.key
   openssl rsa -in /app/configs/jwtRS256.key -pubout -outform PEM -out /app/configs/jwtRS256.key.pub
   "JWT private and public keys are created."
+fi
