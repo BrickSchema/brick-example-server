c3bf26559371da2681fe1a460cdc985acec54e7a
Jason Koh <bk7749@gmail.com>
2020-03-09 06:51:26 +0000

    update

---
+++ brick_server/dummy_frontend.py
@@ -0,0 +1,22 @@
+import time
+
+import arrow
+from fastapi_utils.inferring_router import InferringRouter
+from fastapi import Depends, Header, HTTPException, Body, Query, Path
+from starlette.responses import HTMLResponse, RedirectResponse
+from starlette.requests import Request
+
+from .configs import configs
+from .auth.authorization import create_user, oauth
+
+from pdb import set_trace as bp
+
+frontend_hostname = configs['frontend']['hostname']
+loggedin_frontend = frontend_hostname + '/loggedin_page'
+
+
+dummy_frontend_router = InferringRouter('dummy-frontend')
+
+@dummy_frontend_router.get('/loggedin_page') # TODO: This is supposed to be a main page for the user in the frontend.
+def get_dummy_register_user(request: Request):
+    return 'successful'
---
+++ brick_server/exceptions.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException
+
+
+class BrickServerError(Exception):
+    def __init__(self, *args, **kwargs):
+        super(BrickServerError, self).__init__(*args, **kwargs)
+
+
+class DoesNotExistError(BrickServerError):
+    def __init__(self, klass, name, *args, **kwargs):
+        self.klass = klass
+        self.name = name
+        super(DoesNotExistError, self).__init__(*args, **kwargs)
+
+    def __str__(self):
+        return repr('{0} of {1} does not exist'.format(self.name, self.klass))
+
+class MultipleObjectsFoundError(BrickServerError):
+    def __init__(self, klass, name, *args, **kwargs):
+        self.klass = klass
+        self.name = name
+        super(MultipleObjectsFoundError, self).__init__(*args, **kwargs)
+
+    def __str__(self):
+        return repr('There are multiple isntances of {0} of {1}'.format(self.name, self.klass))
+
+class UserNotApprovedError(BrickServerError, HTTPException):
+    def __init__(self, *args, **kwargs):
+        super(UserNotApprovedError, self).__init__(*args, **kwargs)
+
