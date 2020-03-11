1c8c2a95d383e767bf4bc397304ad7a4cd536c66
Jason Koh <bk7749@gmail.com>
2020-03-11 20:26:04 +0000

    remove garbage

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+from .remote.test_entities import *
+from .remote.test_queries import *
+from .remote.test_timeseries import *
+
+#test_load_ttl()
+#test_delete_an_entity()
+#test_post_timeseries()
+#test_get_timeseries()
+
+#test_simple_sparql()
+#test_get_timeseries()
+#test_simple_sql()
+test_create_entities()
