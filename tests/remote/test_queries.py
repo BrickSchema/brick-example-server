5e95185cab312ede15ac8ff46c2dee08ef968b07
Jason Koh <bk7749@gmail.com>
2020-04-02 21:18:21 +0000

    add an equipment query

--- tests/remote/test_queries.py
+++ tests/remote/test_queries.py
@@ -1,5 +1,5 @@
 import requests
-import pdb
+from pdb import set_trace as bp
 
 from .common import QUERY_BASE, ENTITY_BASE, authorize_headers
 
@@ -37,3 +37,24 @@ select * from brick_data;
     assert resp.status_code == 200
     assert resp.json()
 
+
+def test_sparql_equip_tree():
+    qstr = """
+select ?child ?parent where {
+    {?parent brick:isPartOf ?child.}
+    UNION
+    {?child brick:isPartOf ?parent.}
+    UNION
+    {?parent brick:feeds ?child.}
+    UNION
+    {?child brick:isFedBy ?parent.}
+    ?parent a/rdfs:subClassOf* brick:Equipment.
+    ?child a/rdfs:subClassOf* brick:Equipment.
+}
+"""
+    headers = authorize_headers({
+        'Content-Type': 'sparql-query'
+    })
+    resp = requests.post(QUERY_BASE + '/sparql', data=qstr, headers=headers)
+    assert resp.json()['results']['bindings']
+    bp()
