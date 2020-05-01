54728c2d4e8e5d12f251512127dfd0db9b54fda1
Jason Koh <bk7749@gmail.com>
2020-05-01 23:15:28 +0000

    add location query

--- tests/remote/test_queries.py
+++ tests/remote/test_queries.py
@@ -45,9 +45,9 @@ select ?child ?parent where {
     UNION
     {?child brick:isPartOf ?parent.}
     UNION
-    {?parent bf:feeds ?child.}
+    {?parent brick:feeds ?child.}
     UNION
-    {?child bf:isFedBy ?parent.}
+    {?child brick:isFedBy ?parent.}
     ?parent a/rdfs:subClassOf* brick:Equipment.
     ?child a/rdfs:subClassOf* brick:Equipment.
 }
@@ -57,3 +57,16 @@ select ?child ?parent where {
     })
     resp = requests.post(QUERY_BASE + '/sparql', data=qstr, headers=headers)
     assert len(resp.json()['results']['bindings']) > 2
+
+def test_sparql_location_tree():
+    qstr = """
+select ?parent ?child where {
+  ?parent a/rdfs:subClassOf brick:Location.
+  ?child brick:isPartOf+ ?parent.
+}
+"""
+    headers = authorize_headers({
+        'Content-Type': 'sparql-query'
+    })
+    resp = requests.post(QUERY_BASE + '/sparql', data=qstr, headers=headers)
+    assert len(resp.json()['results']['bindings']) > 4
