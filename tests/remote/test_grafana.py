3c8a8ec29f7b81ce77e8a630f883de69efb7edec
Jason Koh <bk7749@gmail.com>
2020-08-18 16:25:12 -0700

    update

--- tests/remote/test_grafana.py
+++ tests/remote/test_grafana.py
@@ -10,13 +10,13 @@ from .data import *
 
 def test_create_dashbaord():
     headers = authorize_headers()
-    url = GRAFANA_BASE + '/test'
+    url = GRAFANA_BASE + '/'
     resp = requests.post(url, headers=headers)
     assert resp.status_code in [201, 409]
 
 def test_get_dashboard():
     headers = authorize_headers()
-    url = GRAFANA_BASE + '/test'
+    url = GRAFANA_BASE + '/'
     resp = requests.get(url, headers=headers)
     assert resp.status_code in [201, 409]
 
