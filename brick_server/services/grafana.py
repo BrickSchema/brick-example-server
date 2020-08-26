5613deac1ff1f66192626dbf7521afc5636ed7ca
Jason Koh <bk7749@gmail.com>
2020-08-25 23:24:37 -0700

    add updating grafana dashboard

--- brick_server/services/grafana.py
+++ brick_server/services/grafana.py
@@ -1,4 +1,5 @@
-import pdb
+from pdb import set_trace as bp
+import json
 from copy import deepcopy
 from uuid import uuid4 as gen_uuid
 from io import StringIO
@@ -14,7 +15,7 @@ from fastapi_utils.inferring_router import InferringRouter
 from fastapi.security import HTTPAuthorizationCredentials
 from starlette.requests import Request
 
-from .models import TimeseriesData, ValueTypes, ValueType, IsSuccess, jwt_security_scheme, GrafanaDashboardResponse
+from .models import TimeseriesData, ValueTypes, ValueType, IsSuccess, jwt_security_scheme, GrafanaDashboardResponse, GrafanaUpdateRequest
 from .models import entity_id_desc, graph_desc, relationships_desc, start_time_desc, end_time_desc
 from .models import value_type_desc, timeseries_data_desc
 from ..helpers import striding_windows
@@ -55,24 +56,11 @@ class GrafanaDashboardResource:
                                         uid=gd.uid
                                         )
 
-    @grafana_router.post('/',
-                     status_code=201,
-                     #description='Get data of an entity with in a time range.',
-                     response_model=GrafanaDashboardResponse,
-                     tags=['Data'],
-                     )
-    @authenticated
-    async def post(self,
-                   token: HTTPAuthorizationCredentials = jwt_security_scheme,
-                   ) -> GrafanaDashboardResponse:
-        payload = parse_jwt_token(token.credentials)
-        user_id = payload['user_id']
-        user = get_doc(User, user_id=user_id)
-
+    async def create_grafana_dashboard(self, user):
         body = {
             'dashboard': {
                 'uid': str(gen_uuid()),
-                'title': user_id,
+                'title': user.user_id,
             }
         }
         resp = await self.grafana.post('/dashboards/db', json=body)
@@ -92,7 +80,59 @@ class GrafanaDashboardResource:
             )
         else:
             if resp.json()['message'] == 'A dashboard with the same name in the folder already exists':
-                raise AlreadyExistsError('Grafana Dashboard', user_id)
+                raise AlreadyExistsError('Grafana Dashboard', user.user_id)
             else:
                 raise HTTPException(detail='unknown error', status_code=500)
+
+    async def update_grafana_dashboard(self, user, grafana_model):
+        gd = get_doc(GrafanaDashboard, user=user)
+        body = {
+            'dashboard': grafana_model
+        }
+        body['dashboard']['uid'] = gd.uid
+        body['dashboard']['id'] = gd.grafana_id
+        body['dashboard']['title'] = user.user_id
+        body['overwrite'] = True
+
+        resp = await self.grafana.post('/dashboards/db', json=body)
+        if resp.status_code == 200:
+            return GrafanaDashboardResponse(
+                url=gd.url,
+                uid=gd.uid,
+                grafana_id=gd.grafana_id,
+            )
+        else:
+            msg = resp.json()['message']
+            if msg == 'A dashboard with the same name in the folder already exists':
+                raise AlreadyExistsError('Grafana Dashboard', user_id)
+            else:
+                raise HTTPException(detail=msg, status_code=500)
+
+
+    @grafana_router.post('/',
+                     status_code=200,
+                         description="Create or update the Grafana Dashboard. If JSON body is not given, it creates a Dashboard and assign it to the user. If JSON body is given, the body should be same as Grafana's dashboard model as defined at `https://grafana.com/docs/grafana/latest/http_api/dashboard/` except that uid, id, and title should be empty.",
+                     response_model=GrafanaDashboardResponse,
+                     tags=['Data'],
+                     )
+    @authenticated
+    async def post(self,
+                   request: Request,
+                   token: HTTPAuthorizationCredentials = jwt_security_scheme,
+                   #grafana_model: GrafanaUpdateRequest = Body(None, description='If given, update the existing Grafana dashboard assigned to the user. Otherwise, create a new Grafana Dashboard for the user.', required=False),
+                   ) -> GrafanaDashboardResponse:
+        payload = parse_jwt_token(token.credentials)
+        user_id = payload['user_id']
+        user = get_doc(User, user_id=user_id)
+
+        body = await request.body()
+        if body:
+            grafana_model = json.loads(body)
+            print('===========================')
+            print(grafana_model)
+            print('===========================')
+            resp = await self.update_grafana_dashboard(user, grafana_model)
+        else:
+            resp = await self.create_grafana_dashboard(user)
+        return resp
 
--- tests/remote/test_grafana.py
+++ tests/remote/test_grafana.py
@@ -1,4 +1,5 @@
 from urllib.parse import quote_plus
+import json
 import time
 import requests
 from pdb import set_trace as bp
@@ -12,11 +13,19 @@ def test_create_dashbaord():
     headers = authorize_headers()
     url = GRAFANA_BASE + '/'
     resp = requests.post(url, headers=headers)
-    assert resp.status_code in [201, 409]
+    assert resp.status_code in [200, 409]
 
 def test_get_dashboard():
     headers = authorize_headers()
     url = GRAFANA_BASE + '/'
     resp = requests.get(url, headers=headers)
+    assert resp.status_code == 200
+
+def test_update_dashboard():
+    headers = authorize_headers()
+    grafana_model = json.load(open('tests/remote/grafana_body.json'))
+
+    url = GRAFANA_BASE + '/'
+    resp = requests.post(url, headers=headers, json=grafana_model)
     assert resp.status_code == 200
 
